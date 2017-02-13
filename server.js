15bc511a409aa9ff10a8d5af69c4c20bb9324677
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2017-02-14 01:13:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res) {
+app.get('article-one', function(req, res) {
     res.sendFile('Article one is requested and served');
 });
 app.get('article-two', function(req, res) {
