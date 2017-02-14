72588137ee77885f9621e8978cb550800d34551e
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2017-02-15 01:06:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,14 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='who r u';
-var img=document.getElementById('rishu');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft+10;
+var img = document.getElementById('rishu');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft=marginLeft + 10;
     img.style.marginLeft=marginLeft + 'px';
     
 }
 
-img.onClick=function()
-{
-    var interval=setInterval(moveRight,50);
+img.onClick=function(){
+    var interval=setInterval(moveRight , 100);
 };
