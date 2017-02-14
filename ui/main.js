7add4d6e8d2f7816129b9d44aa74b4c00fb519a7
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2017-02-15 00:55:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,16 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='wow i am cool';
+var element=document.getElementById('rishu');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+    
+}
+
+var onClick=function()
+{
+    var interval=setInterval(moveRight,50);
+};
