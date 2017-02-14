cdc59f862b9af1b5fb12a79fd64069d3901e51f3
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2017-02-15 00:59:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,11 +6,11 @@ var marginLeft=0;
 function moveRight()
 {
     marginLeft=marginLeft+10;
-    element.style.marginLeft=marginLeft+'px';
+    element.style.marginLeft=marginLeft + 'px';
     
 }
 
-var onClick=function()
+element.onClick=function()
 {
     var interval=setInterval(moveRight,50);
 };
