0cbcdd379ecd618a39c300301c258c750b5fd44c
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2017-02-15 01:11:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='r u';
-var img = document.getElementById('rishu');
+var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
     marginLeft=marginLeft + 10;
