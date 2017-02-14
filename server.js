919396a8b3efa31b2141867a08d7b6b570b7da64
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2017-02-14 11:33:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ var htmlTemplate=`
 <html>
     <head>
         <title>
-            $(title)
+            $(title);
         </title>
         <meta name="viewport" content="width-device-width ,initial-scale=1">
         <link href="/ui/style.css" rel="stylesheet" />
@@ -65,10 +65,10 @@ var htmlTemplate=`
         </div>
         <hr/>
         <h3>
-            $(heading)
+            $(heading);
         </h3>
         <div>
-            $(content)
+            $(content);
         </div>
         </div>
     </body>
