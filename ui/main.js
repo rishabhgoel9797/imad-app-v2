c772801137aa1e764d2eccc74a742e13d28394ad
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2017-02-15 19:43:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,20 @@ var button=document.getElementById('counter');
 var counter =0;
 button.onclick = function()
 {
-    counter = counter +1;
-    var span=document.getElementById('count');
+    var request = XMLHTTPRequest();
+    request.onreadystatechange=function()
+    {if(request.readyState==XMLHTTPReques.DONE)
+    {
+        if(request.status==200)
+        {
+            var counter=request.responseText;
+            var span=document.getElementById('count');
     span.innerHTML=counter.toString();
+        }
+    }
+    };
+    
+    request.open('GET', 'http://rishabhgoel9797.imad.hasura-app.io/',true);
+    request.send(NULL);
+    
 };
