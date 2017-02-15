f9256b0261ee6052110aa3949a55871555b09789
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2017-02-15 23:12:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,12 @@ var htmlTemplate=`
 }
 
 
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
 var counter = 0;
 app.get('/counter', function(req,res){
     counter=counter +1;
@@ -92,9 +98,6 @@ app.get('/submit-name' ,function(req, res) {
    res.send(JSON.stringify(names));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 
 app.get('/:articleName', function(req, res) {
     var articleName=req.params.articleName;
