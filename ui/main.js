de33fc9e9f96d1cd528f1ae01ffc3ebcb359e09f
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2017-02-15 18:50:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,5 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
-element.innerHTML='r u';
 var element = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
