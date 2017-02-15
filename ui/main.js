7ff9a900c4cdddd52ab8ecc9153f480bcc617926
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2017-02-15 23:31:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,11 +22,20 @@ button.onclick = function()
 
 //submit button
 
-var nameInput = document.getElementById('name');
-var name= nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=['name1','name2','name3','name4'];
+    
+      var request = new  XMLHttpRequest();
+    request.onreadystatechange=function()
+    {if(request.readyState===XMLHttpRequest.DONE)
+    {
+        if(request.status===200)
+        {
+        var names=request.responseText;
+        names=JSON.parse(names);
+        
+        
     var list='';
     for(var i=0;i<names.length;i++)
     {
@@ -34,5 +43,15 @@ submit.onclick=function(){
     }
     var ul=document.getElementById('nameList');
     ul.innerHTML=list;
+        }
+    }
+    };
+    
+    
+    var nameInput = document.getElementById('name');
+var name= nameInput.value;
+
+    request.open('GET', 'http://rishabhgoel9797.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
     
 };
